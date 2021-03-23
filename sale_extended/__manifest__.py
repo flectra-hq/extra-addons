09d9ee501f978cb916c6a3bbbb12561f8088ac67
Princy <princy@techreceptives.com>
2021-03-23 16:15:36 +0530

    [ADD]:added new modules and deleted older ones

---
+++ sale_extended/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, Flectra. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Sale Extended',
+    'version': '2.0.1.0',
+    "website": "https://flectrahq.com/",
+    "author": "FlectraHQ",
+    "license": "LGPL-3",
+    'depends': ['sale'],
+    'data': [
+        'views/sale_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
